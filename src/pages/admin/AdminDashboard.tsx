0db460ef278ceb6fb4c6478388a0d5d1cfb69fa5
Hoang Son <sonjiny2004@gmail.com>
2026-01-11 01:24:07 +0700

    fix(deploy bug)

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { DollarSign, ShoppingBag, Package, Users } from 'lucide-react';
 import KPICard from './components/dashboard/KPICard';
 import RevenueChart from './components/dashboard/RevenueChart';
